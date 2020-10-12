6b75f1f9aa5a17cf9ba459035a1ffdedaeb48bd4
renansdf <rnn.frts@gmail.com>
2020-10-12 03:06:23 -0300

    imported new dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
+import 'reflect-metadata';
+import 'express-async-errors';
 import express from 'express';
+import sessionRouter from './modules/session/sessionRouter';
+
+import './connection';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/session', sessionRouter);
 
 app.listen(3333, () => {
   console.log('server started on port 3333');

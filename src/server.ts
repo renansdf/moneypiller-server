5686fd4650f8b7002903f1ec6069c8b581bf6f69
renansdf <rnn.frts@gmail.com>
2020-10-13 23:25:12 -0300

    refactored initialization of app to accomodate socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,25 @@
 import 'reflect-metadata';
 import 'express-async-errors';
 import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import { connectToSocketServer } from './socketConnection';
+
 import sessionRouter from './modules/session/sessionRouter';
+import userRouter from './modules/user/userRouter';
 
 import './connection';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use('/session', sessionRouter);
+app.use('/user', userRouter);
 
-app.listen(3333, () => {
+const server = http.createServer(app);
+connectToSocketServer(server);
+
+server.listen(3333, () => {
   console.log('server started on port 3333');
 });

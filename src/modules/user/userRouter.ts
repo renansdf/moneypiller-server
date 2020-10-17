8366c98fd1db4f6ed65c7c1b4d3395165739ff6d
renansdf <rnn.frts@gmail.com>
2020-10-17 16:06:39 -0300

    removed database functionality that saved user on mongo db atlas

--- src/modules/user/userRouter.ts
+++ src/modules/user/userRouter.ts
@@ -2,12 +2,23 @@ import { Router, Request, Response } from 'express';
 import { registerUserData } from '../../socketConnection';
 import { createUser } from './typeorm/userRepository';
 
+interface IUserData {
+  socket_id: string;
+  name: string;
+  session_hash: string;
+  monthly_cost: number;
+  monthly_profit: number;
+  balance: number;
+}
+
 const userRouter = Router();
 
 userRouter.post('/', async (request: Request, response: Response) => {
-  const { socketId, name, session_hash, monthly_cost, monthly_profit, balance } = request.body;
+  const { socket_id, name, session_hash, monthly_cost, monthly_profit, balance } = request.body;
 
-  const user = await createUser({ socketId, name, session_hash, monthly_cost, monthly_profit, balance });
+  // const user = await createUser({ socketId, name, session_hash, monthly_cost, monthly_profit, balance });
+
+  const user: IUserData = { socket_id, name, session_hash, monthly_cost, monthly_profit, balance }
 
   registerUserData(user);
 

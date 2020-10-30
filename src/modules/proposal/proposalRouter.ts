8f5b4f50fa4065de8fe1a04f9e36405b969200a1
renansdf <rnn.frts@gmail.com>
2020-10-30 19:39:41 -0300

    updated post route and create put route to update validators list

--- src/modules/proposal/proposalRouter.ts
+++ src/modules/proposal/proposalRouter.ts
@@ -1,27 +1,43 @@
 import { Router, Request, Response } from 'express';
-import { updateProposals } from '../../socketConnection';
+import { addValidatorToProposal, updateProposals } from '../../socketConnection';
 
 interface IProposal {
   name: string;
   value: number;
 }
 
+interface IUsersValidated {
+  validator_name: string;
+  validator_socket_id: string;
+}
 interface IUserProposalData {
   from: string;
   socket_id: string;
   user_proposals: IProposal[];
+  users_validated: IUsersValidated[];
 }
 
 const proposalRouter = Router();
 
 proposalRouter.post('/', async (request: Request, response: Response) => {
-  const { from, socket_id, user_proposals } = request.body;
+  const { from, socket_id, user_proposals, users_validated } = request.body;
 
-  const userProposal: IUserProposalData = { socket_id, from, user_proposals }
+  const userProposal: IUserProposalData = { socket_id, from, user_proposals, users_validated }
 
   updateProposals(userProposal);
 
   return response.json(userProposal);
+});
+
+proposalRouter.put('/', async (request: Request, response: Response) => {
+  const { proposal_socket_id, add_name, add_socket_id } = request.body;
+
+  addValidatorToProposal({
+    validator_name: add_name,
+    validator_socket_id: add_socket_id
+  }, proposal_socket_id);
+
+  return response.json({ add_name, add_socket_id, proposal_socket_id });
 });
 
 export default proposalRouter;

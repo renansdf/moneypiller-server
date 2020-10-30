e173c8ffe0a14740591ce88fe10664c39c15d824
renansdf <rnn.frts@gmail.com>
2020-10-30 19:42:13 -0300

    created disconnect funciont, updated emits on connect, created function to handle proposal validation

--- src/socketConnection.ts
+++ src/socketConnection.ts
@@ -1,6 +1,5 @@
 import { Server } from 'http';
 import socketio, { Server as IServer } from 'socket.io';
-import { ObjectID } from 'typeorm';
 
 interface IUserData {
   socket_id: string;
@@ -16,15 +15,22 @@ interface IProposal {
   value: number;
 }
 
+interface IUsersValidated {
+  validator_name: string;
+  validator_socket_id: string;
+}
+
 interface IUserProposalData {
   from: string;
   socket_id: string;
   user_proposals: IProposal[];
+  users_validated: IUsersValidated[];
 }
 
 export let socketServer: IServer;
 export let connectedUsers: IUserData[] = [];
 export let allProposals: IUserProposalData[] = [];
+export let pileValue: number = 0;
 
 export function connectToSocketServer(server: Server) {
   socketServer = socketio(server);
@@ -34,15 +40,23 @@ export function connectToSocketServer(server: Server) {
 
     socket.emit('userConnection', 'a user connecteds');
 
+    socket.emit('updateUsers', connectedUsers);
 
-    // ON DISCONECT, DELETE USER FROM 
-    // USING THE SOCKET ID
+    socket.emit('updateProposals', allProposals);
 
-    // socket.on('disconnect', () => {
-    //   socket.id;
-    //   delete connectedUsers[socket.id];
-    // });
+    socket.emit('updatePileValue', pileValue)
+
+    socket.on('disconnect', () => {
+      socket.id;
+      connectedUsers = connectedUsers.filter(user => user.socket_id !== socket.id);
+
+      allProposals = allProposals.filter(proposal => proposal.socket_id !== socket.id);
+
+      socketServer.emit('updateUsers', connectedUsers);
+      socketServer.emit('updateProposals', allProposals);
+    });
   });
+
 }
 
 export function registerUserData(data: IUserData) {
@@ -59,4 +73,25 @@ export function updateProposals(data: IUserProposalData) {
 
   allProposals = filteredProposals;
   socketServer.emit('updateProposals', allProposals);
+}
+
+export function addValidatorToProposal(newUser: IUsersValidated, proposal_socket_id: string) {
+  const updatedProposals = allProposals.map(proposal => {
+    if (proposal.socket_id === proposal_socket_id) {
+      let updatedUserList = proposal.users_validated.filter(user => {
+        return user.validator_socket_id !== newUser.validator_socket_id
+      });
+      updatedUserList.push(newUser);
+      proposal.users_validated = updatedUserList;
+    }
+    return proposal;
+  });
+
+  allProposals = updatedProposals;
+  socketServer.emit('updateProposals', allProposals);
+}
+
+export function updatePile(newPileValue: number) {
+  pileValue = newPileValue;
+  socketServer.emit('updatePileValue', pileValue);
 }
